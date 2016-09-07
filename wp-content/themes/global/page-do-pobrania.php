f7b63a54033795a9150e5171110c31a94342e000
Gromek Marcin <mgromek@efigence.com>
2016-09-07 18:34:22 +0200

    add 'do pobrania' page

---
+++ wp-content/themes/global/page-do-pobrania.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * Template name: Do pobrania
+ *
+ */
+
+get_header(); ?>
+
+<div class="small-12 column">
+	<table class="row">
+		<thead class="small-12 column">
+			<tr class="row text-center">
+				<th class="small-2 text-center column">Typ</th>
+				<th class="small-10 column">Plik</th>
+			</tr>
+		</thead>
+		<tbody class="small-12 column">
+			<tr class="row align-middle">
+				<td class="small-2 text-center column">
+					<span class="icon-file-<?php echo strtolower( pathinfo( get_field('file_1_file'), PATHINFO_EXTENSION ) )?> icon-2x" aria-hidden="true"></span>
+				</td>
+				<td class="small-10 column">
+					<a href="<?php the_field('file_1_file'); ?>" download="<?php echo basename(get_field('file_1_file') ); ?>"><?php the_field('file_1_name'); ?></a>
+				</td>
+			</tr>
+			<tr><a href=""></a></tr>
+		</tbody>
+	</table>
+
+
+</div>
+
+<?php get_footer(); ?>
