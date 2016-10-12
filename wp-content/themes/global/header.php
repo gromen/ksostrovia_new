2df2f2b68e1d0692280a42de1e20ea7358c30d04
Gromek Marcin <mgromek@efigence.com>
2016-10-12 20:51:57 +0200

    update plugins

--- wp-content/themes/global/header.php
+++ wp-content/themes/global/header.php
@@ -94,15 +94,11 @@
 			        <div class="swiper-slide"><img src="<?php bloginfo('template_directory') ?>/src/images/banner1.jpg" alt=""></div>
 			        ...
 			    </div>
-			    <!-- If we need pagination -->
-			    <div class="swiper-pagination"></div>
 
 			    <!-- If we need navigation buttons -->
 			    <div class="swiper-button-prev"></div>
 			    <div class="swiper-button-next"></div>
 
-			    <!-- If we need scrollbar -->
-			    <div class="swiper-scrollbar"></div>
 			</div>
 
 			<main id="content" class="site-content">
